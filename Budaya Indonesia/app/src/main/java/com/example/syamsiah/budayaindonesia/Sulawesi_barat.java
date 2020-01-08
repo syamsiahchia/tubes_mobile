aabae5b2ada62dde06d9272c52e88a572f19ec26
abulkahfi <60900117030@uin-alauddin.ac.id>
2020-01-08 22:32:41 +0800

    Cia

---
+++ Budaya Indonesia/app/src/main/java/com/example/syamsiah/budayaindonesia/Sulawesi_barat.java
@@ -0,0 +1,16 @@
+package com.example.syamsiah.budayaindonesia;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by HP on 4/6/2016.
+ */
+public class Sulawesi_barat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sulawesi_barat);
+    }
+}
---
+++ Budaya Indonesia/app/src/main/java/com/example/syamsiah/budayaindonesia/Sulawesi_utara.java
@@ -0,0 +1,15 @@
+package com.example.syamsiah.budayaindonesia;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by HP on 4/6/2016.
+ */
+public class Sulawesi_utara extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sulawesi_utara);
+    }
+}
